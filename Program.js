6195ccc3004b487997d61dab5369555c5b0f8b57
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-26 23:18:06 +0200

    Start

---
+++ Program.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/koniec') {
+            process.stdout.write('Quiting app!\n');
+            process.exit();
+        }
+        else {
+            process.stderr.write('Wrong instruction!\n');
+        };
+        switch (instruction) {
+            case '/version':
+             process.env();   
+                break;
+        }
+    }
+    
+});
