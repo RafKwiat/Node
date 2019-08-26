8ccc53158feff1b259892de4f8b991851dd55110
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-26 23:52:05 +0200

    First task

--- Program.js
+++ Program.js
@@ -4,18 +4,20 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/koniec') {
-            process.stdout.write('Quiting app!\n');
-            process.exit();
-        }
-        else {
-            process.stderr.write('Wrong instruction!\n');
-        };
-        switch (instruction) {
-            case '/version':
-             process.env();   
+        
+        switch(instruction) {
+                case '/exit':
+                    process.stdout.write('Quitting app!\n');
+                    process.exit();
+                break;
+                case 'specs':
+                    console.log('Node version: ' + process.versions.node);
+                    console.log('System language: ' + process.env.lang);
+                    getOSinfo(); //funkcja którą powinieneś stworzyć później w 17.3
+                break;
+                default:
+                    process.stderr.write('Wrong instruction!\n');
                 break;
         }
     }
-    
 });
